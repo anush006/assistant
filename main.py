35bcf504b0aaf2f1b4933348ef37c91b368eacdf
Anush <anushoffcl@gmail.com>
2025-01-31 21:13:21 +0530

    Changes

--- main.py
+++ main.py
@@ -4,44 +4,40 @@ from elevenlabs.client import ElevenLabs
 
 speak_client = ElevenLabs(api_key="sk_f6844abb8728b9950c19ade00376fe2e918b29a39603bb11")
 model_client = InferenceClient(api_key="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")
-device = "cuda" if torch.cuda.is_available() else "cpu"
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 def speak(text):
-    # audio = speak_client.generate(
-    #     text=text,
-    #     model="eleven_turbo_v2_5",
-    #     voice="Chris"
-    # )
-    # play(audio)
-
-    wav = tts.tts(text=text, speaker_wav="my/cloning/audio.wav", language="en")
-    print(wav)
+    audio = speak_client.generate(
+        text=text,
+        model="eleven_turbo_v2_5",
+        voice="Chris"
+    )
+    play(audio)
 
 prompt = ''
 
-while prompt!="bye":
-    prompt = input("ENter text: ")
-    speak(prompt)
-# while prompt!= "bye":
+# while prompt!="bye":
+#     prompt = input("Enter text: ")
+#     speak(prompt)
+    
+while prompt!= "bye":
 
-#     prompt = input("\nPrompt: ")
+    prompt = input("\nPrompt: ")
 
-#     messages = [
-#         {
-#             "role": "user",
-#             "content": prompt
-#         }
-#     ]
-#     try:
-#         completion = model_client.chat.completions.create(
-#             model="microsoft/Phi-3.5-mini-instruct", 
-#             messages=messages, 
-#             max_tokens=500
-#         )
+    messages = [
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+    try:
+        completion = model_client.chat.completions.create(
+            model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", 
+            messages=messages, 
+            max_tokens=500
+        )
 
-#         print(completion.choices[0].message.content)
-#         speak(completion.choices[0].message.content)
+        print(completion.choices[0].message.content)
+        # speak(completion.choices[0].message.content)
 
-#     except Exception as e:
-#         print(e)
+    except Exception as e:
+        print(e)
